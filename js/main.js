17551ab2bfd0f8e81d48b13e83261fa677e2aa59
Dr.MohamedSaleh <mohamadsaleh76@MacBook-Pro-alkhas-b-DrMohamedSaleh.local>
2022-06-08 14:49:00 +0300

    hi

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// let navLink = document.querySelectorAll(".nav-link")
+// navLink.forEach(element => {
+    
+//     element.addEventListener('mouseenter', () =>{
+//         element.classList.add("actived")
+//     })
+//     element.addEventListener("mouseleave",() =>{
+//         element.classList.remove("actived")  
+//     })
+    
+// });
+let aside = document.querySelectorAll("aside")[0]
+let btn = document.querySelectorAll("header .left-section i")[0]
+btn.onclick = ()=>{
+    if(aside.className == "shown"){
+        aside.classList.remove("shown")
+    }else{
+        aside.classList.add("shown")
+    }
+    
+}
